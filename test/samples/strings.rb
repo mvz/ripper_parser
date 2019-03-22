951e0b5bb1dc88c5246e6e8a91af03e5761bdbf3
Matijs van Zuijlen <matijs@matijs.net>
2019-03-22 11:40:41 +0100

    Update samples to match latest ripper_ruby_parser

--- test/samples/strings.rb
+++ test/samples/strings.rb
@@ -11,12 +11,15 @@
 "foo#{bar}baz\0"
 "2\302\275"
 "#{foo}2\302\275"
+%W(2\302\275)
+/2\302\275/
 
 # Encoding
 "日本語"
+/日本語/
 "\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E"  # => "日本語"
 
- # Invalid in UTF8
+# Invalid in UTF8
 # "\xAB\xE6\x97\xA5"
 
 <<EOS
