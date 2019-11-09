b0f68f2089587d39009af33fccfa31dfb312676c
Matijs van Zuijlen <matijs@matijs.net>
2019-11-09 19:46:05 +0100

    Add option with_line_numbers to assert_parsed_as_before

--- test/end_to_end/line_numbering_test.rb
+++ test/end_to_end/line_numbering_test.rb
@@ -3,49 +3,6 @@
 require File.expand_path("../test_helper.rb", File.dirname(__FILE__))
 
 describe "Using RipperParser and Parser" do
-  def to_line_numbers(exp)
-    exp.map! do |sub_exp|
-      if sub_exp.is_a? Sexp
-        to_line_numbers sub_exp
-      else
-        sub_exp
-      end
-    end
-
-    if exp.sexp_type == :scope
-      exp
-    else
-      s(:line_number, exp.line, exp)
-    end
-  end
-
-  def to_line_numbers_ast(exp)
-    type = exp.type
-    children = exp.children.map do |sub_exp|
-      if sub_exp.is_a? AST::Node
-        to_line_numbers_ast sub_exp
-      else
-        sub_exp
-      end
-    end
-
-    mapped = s(type, *children)
-
-    if type == :scope
-      mapped
-    else
-      s(:line_number, exp.location.line, mapped)
-    end
-  end
-
-  let :newparser do
-    RipperParser::Parser.new
-  end
-
-  let :oldparser do
-    Parser::CurrentRuby
-  end
-
   describe "for a multi-line program" do
     let :program do
       <<-RUBY
@@ -62,21 +19,12 @@ describe "Using RipperParser and Parser" do
       RUBY
     end
 
-    let :original do
-      oldparser.parse program
-    end
-
-    let :imitation do
-      newparser.parse program
-    end
-
     it "gives the same result" do
-      _(formatted(imitation)).must_equal formatted(original)
+      _(program).must_be_parsed_as_before
     end
 
     it "gives the same result with line numbers" do
-      _(formatted(to_line_numbers(imitation)))
-        .must_equal formatted(to_line_numbers_ast(original))
+      _(program).must_be_parsed_as_before with_line_numbers: true
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,27 +18,79 @@ Parser::Builders::Default.emit_index    = true
 
 module MiniTest
   class Spec
-    def formatted(exp)
-      exp.inspect.gsub(/^  */, "").gsub(/, s\(/, ",\ns(").gsub(/\), /, "),\n")
+    def inspect_with_line_numbers(exp)
+      parts = exp.map do |sub_exp|
+        if sub_exp.is_a? Sexp
+          inspect_with_line_numbers(sub_exp)
+        else
+          sub_exp.inspect
+        end
+      end
+
+      plain = "s(#{parts.join(', ')})"
+      if (line = exp.line)
+        "#{plain}.line(#{line})"
+      else
+        plain
+      end
     end
 
-    def assert_parsed_as(sexp, code)
+    def inspect_with_line_numbers_ast(exp)
+      parts = exp.children.map do |sub_exp|
+        if sub_exp.is_a? AST::Node
+          inspect_with_line_numbers_ast(sub_exp)
+        else
+          sub_exp.inspect
+        end
+      end
+      
+      parts.unshift exp.type.inspect
+
+      plain = "s(#{parts.join(', ')})"
+      if (line = exp.location.line)
+        "#{plain}.line(#{line})"
+      else
+        plain
+      end
+    end
+
+    def formatted(exp, with_line_numbers: false)
+      inspection = if with_line_numbers
+                     inspect_with_line_numbers(exp)
+                   else
+                     exp.inspect
+                   end
+      inspection.gsub(/^  */, "").gsub(/, s\(/, ",\ns(").gsub(/\), /, "),\n")
+    end
+
+    def formatted_ast(exp, with_line_numbers: false)
+      inspection = if with_line_numbers
+                     inspect_with_line_numbers_ast(exp)
+                   else
+                     exp.inspect
+                   end
+      inspection.gsub(/^  */, "").gsub(/, s\(/, ",\ns(").gsub(/\), /, "),\n")
+    end
+
+    def assert_parsed_as(sexp, code, with_line_numbers: false)
       parser = RipperParser::Parser.new
       result = parser.parse code
       if sexp.nil?
         assert_nil result
       else
         assert_equal sexp, result
-        assert_equal sexp.to_s, result.to_s
+        assert_equal(formatted(sexp, with_line_numbers: with_line_numbers),
+                     formatted(result, with_line_numbers: with_line_numbers))
       end
     end
 
-    def assert_parsed_as_before(code)
+    def assert_parsed_as_before(code, with_line_numbers: false)
       oldparser = Parser::CurrentRuby
       newparser = RipperParser::Parser.new
       expected = oldparser.parse code.dup
       result = newparser.parse code
-      assert_equal formatted(expected), formatted(result)
+      assert_equal(formatted_ast(expected, with_line_numbers: with_line_numbers),
+                   formatted(result, with_line_numbers: with_line_numbers))
     end
   end
 
