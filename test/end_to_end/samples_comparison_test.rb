7c04487bf8062c61ec662a2a9d5c32898ecd7462
Matijs van Zuijlen <matijs@matijs.net>
2021-08-02 10:02:13 +0200

    Add Ruby 3.0 specific right-ward assignment sample

--- test/end_to_end/samples_comparison_test.rb
+++ test/end_to_end/samples_comparison_test.rb
@@ -6,6 +6,7 @@ describe "Using RipperParser and Parser" do
   Dir.glob(File.expand_path("../samples/*.rb", File.dirname(__FILE__))).each do |file|
     next if RUBY_VERSION < "2.6.0" && file.match?(/_26.rb\Z/)
     next if RUBY_VERSION < "2.7.0" && file.match?(/_27.rb\Z/)
+    next if RUBY_VERSION < "3.0.0" && file.match?(/_30.rb\Z/)
 
     it "gives the same result for #{file}" do
       program = File.read file
