2b5d47d069573ecf89d0731867b2a99f2094e213
Matijs van Zuijlen <matijs@matijs.net>
2023-06-17 12:54:58 +0200

    Add more assignment samples

--- test/samples/assignment.rb
+++ test/samples/assignment.rb
@@ -1,10 +1,39 @@
 # Samples of assignment
 foo[bar] = baz
 foo[bar] += baz
-foo[bar] ||= foo bar
-foo[bar] ||= foo(bar)
+foo[bar] += baz qux
+foo[bar] += baz(qux)
+foo[bar] += baz.qux quuz
+foo[bar] += baz.qux(quuz)
+foo[bar] ||= baz
+foo[bar] ||= baz qux
+foo[bar] ||= baz(qux)
 foo[bar] ||= baz.qux quuz
 foo[bar] ||= baz.qux(quuz)
+
+foo.bar = baz
+foo.bar += baz
+foo.bar += baz qux
+foo.bar += baz(qux)
+foo.bar += baz.qux quuz
+foo.bar += baz.qux(quuz)
+foo.bar ||= baz
+foo.bar ||= baz qux
+foo.bar ||= baz(qux)
+foo.bar ||= baz.qux quuz
+foo.bar ||= baz.qux(quuz)
+
+foo = baz
+foo += baz
+foo += baz qux
+foo += baz(qux)
+foo += baz.qux quuz
+foo += baz.qux(quuz)
+foo ||= baz
+foo ||= baz qux
+foo ||= baz(qux)
+foo ||= baz.qux quuz
+foo ||= baz.qux(quuz)
 
 # Destructuring assignments
 foo, bar = baz
