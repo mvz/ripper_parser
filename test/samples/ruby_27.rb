c3881bc6d6e3cca658bc637856692d6479befbe5
Matijs van Zuijlen <matijs@matijs.net>
2023-06-17 12:35:06 +0200

    Move pattern matching samples to conditionals

--- test/samples/ruby_27.rb
+++ test/samples/ruby_27.rb
@@ -1,40 +1,5 @@
 # Samples that need Ruby 2.7 or higher
 
-# Pattern matching (experimental)
-case foo
-  in blub
-    p blub
-end
-
-case foo
-  in [bar, baz]
-    quz = bar + baz
-end
-
-case foo
-  in [bar, baz]
-    quz = bar + baz
-  in blub
-    p blub
-end
-
-case foo
-  in { bar: [baz, qux] }
-    quz = bar(baz) + baz
-end
-
-case { foo: 1, bar: 2 }
-  in { bar: }
-    baz bar
-end
-
-case foo
-  in bar, *baz then quz(bar, baz)
-end
-
-# One-line pattern matching (experimental)
-1 in foo
-1 in foo => bar
 
 # Numbered parameters (experimental)
 foos.each { foo _1 }
